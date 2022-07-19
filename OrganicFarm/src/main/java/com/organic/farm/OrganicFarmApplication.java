6f0db3ea7536b3c9b0b40ec593fdf41f7ca19eff
Rahul Makwana <rahulmakwana32@gmail.com>
2022-07-18 23:59:52 -0700

    commit

---
+++ OrganicFarm/src/main/java/com/organic/farm/OrganicFarmApplication.java
@@ -0,0 +1,14 @@
+package com.organic.farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganicFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganicFarmApplication.class, args);
+		System.out.print("test");
+	}
+
+}
---
+++ OrganicFarm/src/test/java/com/organic/farm/OrganicFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organic.farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganicFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
